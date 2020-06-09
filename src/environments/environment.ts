35ff96d06a53947b7e05301f3c4c77f06851a430
adrianf823 <adrianf823@gmail.com>
2020-06-09 07:01:26 +0200

    relaciones y fotos firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyCjc3up5Zg6TQ0FnNyF-gYdCxIXgi_TcTo",
+    authDomain: "proyecto-integrado-imagenes.firebaseapp.com",
+    databaseURL: "https://proyecto-integrado-imagenes.firebaseio.com",
+    projectId: "proyecto-integrado-imagenes",
+    storageBucket: "proyecto-integrado-imagenes.appspot.com",
+    messagingSenderId: "606180481528",
+    appId: "1:606180481528:web:2e6ec7ef6e449b755ade0d",
+    measurementId: "G-07M0N31T8S"
+  }
 };
 
 /*
