a1415a8c4d9126072e12cc68f85b1a5e25a98556
Juan Rodriguez Quijada <juanrodriguezquijada@gmail.com>
2020-06-10 22:00:33 +0200

    enviorement

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
+  production: false,
   firebaseConfig : {
     apiKey: "AIzaSyCjc3up5Zg6TQ0FnNyF-gYdCxIXgi_TcTo",
     authDomain: "proyecto-integrado-imagenes.firebaseapp.com",
